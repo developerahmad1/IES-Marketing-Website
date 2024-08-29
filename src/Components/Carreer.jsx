b47aeb761239c91a5fc82c321dd736eed0cf1d82
Ahmad Nadeem <developerahmad3@gmail.com>
2024-08-29 16:25:34 +0500

    add hiring post and video

--- src/Components/Carreer.jsx
+++ src/Components/Carreer.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import hiringPost from "../assets/hiring post.jpg"
+import hiringVideo from "../assets/HIRING REEL .mp4"
 
 const Carreer = () => {
   return (
     <div>
-      <h1 className='text-4xl text-center my-[100px] text-black mt-[130px] bg-white'>Now There is no jobs are available in Our Company.</h1>
+      <h1 className='text-4xl text-center mt-[130px] text-black  bg-white'>Career</h1>
+      <p className='text-center text-lg my-[20px] '>Currently, we are looking for Sales executive, video editor, and graphic designer. <br /> <span className='font-bold'>Apply now at : </span> 0333-8400116,    0316-6700948</p>
+      <div className='w-full flex justify-center items-center flex-wrap'>
+        <img src={hiringPost} alt="" className='max:w-full w-[500px] mx-auto ' />
+
+        <video src={hiringVideo} className='max:w-full w-[300px] mx-auto' autoPlay muted loop autoFocus controls>
+
+        </video>
+      </div>
     </div>
   )
 }
