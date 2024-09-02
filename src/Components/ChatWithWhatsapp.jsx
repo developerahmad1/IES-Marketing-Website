09b0815e7f6852bb79b80c5a953f0e474a9f6cb6
Ahmad Nadeem <developerahmad3@gmail.com>
2024-09-02 10:22:53 +0500

    set whatsapp icon positon

--- src/Components/ChatWithWhatsapp.jsx
+++ src/Components/ChatWithWhatsapp.jsx
@@ -7,7 +7,7 @@ const ChatWithWhatsapp = () => {
 
     return (
         <a target="_main" href='https://api.whatsapp.com/send/?phone=923054474602&text=Hi+Sir%21+I+am+%3CYour+Name%3E+.+I+am+Interested+in+Real+Estate.+Can+I+get+more+information+about+your+projects.&type=phone_number&app_absent=0'>
-            <div className='fixed sm:bottom-[125px] bottom-[90px] right-[8px] sm:right-[25px]'>
+            <div className='fixed sm:bottom-[125px] bottom-[100px] right-[8px] sm:right-[25px]'>
                 <div className='flex justify-center items-center'>
                     {isHoverOnButton && (
                         <div className='text-[10px] cursor-pointer py-3 px-2 pr-10 bg-white shadow-2xl border rounded-sm transition-all duration-[0.5s] hover:border-blue-800 hover:shadow-2xl '>Chat via Whatsapp</div>
