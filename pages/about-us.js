63879910d9cdd14bafdd68a09ca8e886aa4cb89d
Ahmad Nadeem <developerahmad3@gmail.com>
2024-10-23 10:25:07 +0500

    add sir ayyaz pic

--- pages/about-us.js
+++ pages/about-us.js
@@ -434,7 +434,7 @@ const About = () => {
                 <a href="#" className="flex justify-center items-center">
                   <Image
                     className="max:h-[100px] w-[400px] sm:rounded-t-none rounded-t-[15px] my-0"
-                    src={manFaceIcon}
+                    src={sirAyyaz}
                     alt="Sofia Avatar"
                   />
                 </a>
