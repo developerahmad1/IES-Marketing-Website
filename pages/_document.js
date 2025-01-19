723b10a67e2ac7ffec94bd70b0337b3dd4266447
Ahmad <developerahmad3@gmail.com>
2025-01-19 15:12:20 +0500

    change chat bot

--- pages/_document.js
+++ pages/_document.js
@@ -16,27 +16,25 @@ export default function Document() {
         <Main />
         <NextScript />
 
+        {/* developerahnad3@gmail.com   account */}
         {/* Tidio script */}
-        <Script
+        {/* <Script
           src="//code.tidio.co/q3zgrjfcp807kti0igmfz5faevcsf7yn.js"
           strategy="lazyOnload"
-        />
+        /> */}
 
+        {/* ies marketing account */}
+        <Script
+          src="//code.tidio.co/jprgtnbptp5albyn0n8b7hwenjn2dkbr.js"
+          strategy="lazyOnload"
+        />
       </body>
     </Html>
   );
 }
 
-
-
-
-
-
-
-
 // SEO Bot
 // https://chatgpt.com/c/6721c973-90ec-8001-91ee-864d33c83ee1
-
 
 // seo New Bot
 // https://chatgpt.com/c/67246a99-3284-8001-8931-29ee741233c8
